6e30a3c98a4e11a77bb66f623510cda94f08dd3b
Usov Dmytro <dioks.2013@gmail.com>
2023-08-04 12:59:57 +0300

    add model comment, fix field content in model Post

--- mini_twitter/posts/models.py
+++ mini_twitter/posts/models.py
@@ -5,9 +5,20 @@ from ..users.models import User
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    content = models.EmailField()
+    content = models.TextField()
 
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.created_at
