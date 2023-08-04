c3ba7a49ce878ac5ea144a5f686a6dd9cbaf6bed
Usov Dmytro <dioks.2013@gmail.com>
2023-08-04 14:16:06 +0300

    fix unique field username, email in model User

--- mini_twitter/users/models.py
+++ mini_twitter/users/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class User(models.Model):
-    username = models.CharField(max_length=128)
-    email = models.EmailField()
+    username = models.CharField(max_length=128, unique=True)
+    email = models.EmailField(max_length=200, unique=True)
 
     date_joined = models.DateField(auto_now_add=True)
 
