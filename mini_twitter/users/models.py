b4799f46167af85cadfd4dda7704749df2503686
Usov Dmytro <dioks.2013@gmail.com>
2023-08-04 12:43:20 +0300

    add model user

--- mini_twitter/users/models.py
+++ mini_twitter/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=128)
+    email = models.EmailField()
+
+    date_joined = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
