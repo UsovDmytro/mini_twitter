f9948f396e9b4ff17a05b2332679cbc1022680f3
Usov Dmytro <dioks.2013@gmail.com>
2023-08-07 15:54:05 +0300

    add views for user posts

--- mini_twitter/posts/urls.py
+++ mini_twitter/posts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import posts_list, comments_list
+from .views import posts_list, comments_list, user_posts
 
 urlpatterns = [
     path('', posts_list, name='posts'),
+    path('<int:user_id>/', user_posts, name='user_posts'),
     path('comments/', comments_list, name='comments'),
 ]
--- mini_twitter/posts/views.py
+++ mini_twitter/posts/views.py
@@ -1,14 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post, Comment
 from django.db.models import Count
+from users.models import User
 
 def posts_list(request):
     posts = Post.objects.all()
-    full_posts = []
-    for post in posts:
-        full_posts.append((post, Comment.objects.filter(post__id=post.id).aggregate(Count("id"))['id__count']))
+    full_posts = get_full_posts(posts)
+    title = 'Список постів'
+    context = {'full_posts': full_posts, 'title': title}
+    return render(request, "posts/posts_list.html", context)
 
-    context = {'full_posts': full_posts, 'title': 'Список постів'}
+
+def user_posts(request, user_id):
+    posts = Post.objects.filter(user__id=user_id)
+    full_posts = get_full_posts(posts)
+    title = f'Список постів користувача {get_object_or_404(User, pk=user_id)}'
+    context = {'full_posts': full_posts, 'title': title}
     return render(request, "posts/posts_list.html", context)
 
 
@@ -16,8 +23,14 @@ def comments_list(request):
     posts = Comment.objects.all().values('post').distinct()
     comments_post = []
     for post in posts:
-        print(Post.objects.get(id=post['post']))
         comments_post.append((Post.objects.get(id=post['post']), Comment.objects.filter(post__id=post['post'])))
 
     context = {'comments_post': comments_post, 'title': 'Список коментів'}
     return render(request, "posts/comments_list.html", context)
+
+
+def get_full_posts(posts):
+    full_posts = []
+    for post in posts:
+        full_posts.append((post, Comment.objects.filter(post__id=post.id).aggregate(Count("id"))['id__count']))
+    return full_posts
